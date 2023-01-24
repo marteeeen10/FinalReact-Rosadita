9a8c03ee38657deb594fb76551abc5c56c674dd8
Martin <marteeeen@gmail.com>
2023-01-24 02:08:42 -0300

    first

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar/Navbar";
+import ItemListContainer from "./ItemListContainer/ItemListContainer";
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <ItemListContainer saludo={'Hey Dudeeeeee!'}/>
+    </>
+  );
+}
+
+export default App;
