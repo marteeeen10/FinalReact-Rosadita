2f589902ede066fa8e01bd9f27ad701aea30e8ec
Martin <marteeeen@gmail.com>
2023-03-03 02:51:34 -0300

    clase 9

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import 'react-toastify/dist/ReactToastify.css'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+//Toast
+import { ToastContainer } from 'react-toastify';
 
 import Navbar from "./Navbar/Navbar";
 // import { ItemCount } from "./ItemCount/ItemCount";
 import { ItemListContainer } from "./ItemListContainer/ItemListContainer";
 import { ItemDetailContainer } from './ItemDetailContainer/ItemDetailContainer';
+import { Contacto } from './Contacto/Contacto';
 
 const App = () => {
   
@@ -18,7 +22,9 @@ const App = () => {
         <Route path='/' element={<ItemListContainer/>}/> 
         <Route path='/item/:id' element={<ItemDetailContainer/>}/>
         <Route path='/category/:idCategoria' element={<ItemListContainer/>}/>
+        <Route path='/contacto' element={<Contacto/>}/> 
       </Routes> 
+      <ToastContainer/>
     </BrowserRouter>
 
     </>
--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react"
+import { toast } from 'react-toastify'
 export const ItemCount = ({valInicial, stock}) => {
-    
 
     const [contador, setContador] = useState(valInicial)
     const sumar = () => (contador < stock) && setContador(contador + 1)
     const restar = () => (contador > valInicial) && setContador(contador - 1)
-
+    const agregarCarrito = () =>toast.success ("Articulo Agregado")
 
 
     return (
         <>
-            <button className="btn btn-success" onClick={() => sumar() }>+</button>
+            <button className="btn btn-danger" onClick={() => restar() }>-</button>
             {contador}
-            <button className="btn btn-danger" onClick={() => restar()}>-</button>
+            <button className="btn btn-success" onClick={() => sumar()}>+</button>
+            <button className="btn btn-dark" onClick={() => agregarCarrito()}>Agregar al Carrito</button>
         </>
     );
 }
